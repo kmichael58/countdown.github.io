e043df6ad79aa421522082e36399b0f2d79a468d
kmichael58 <164901685+kmichael58@users.noreply.github.com>
2024-08-22 13:46:26 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+var timer;
+var compareDate = new Date();
+compareDate.setDate(compareDate.getDate() + 7);
+
+timer = setInterval(function (){
+  timeBetweenDates(compareDate);
+}, 1000);
+
+function timeBetweenDates(toDate) {
+  var dateEntered = toDate;
+  var now = new Date();
+  var diffrence = dateEntered.getTime() - now.getTime();
+
+  if(diffrence <= 0) {
+    clearInterval(timer);
+  } else {
+    var seconds = Math.floor(diffrence / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+
+    hours %= 24;
+    minutes %= 60;
+    seconds %= 60;
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+  }
+}
